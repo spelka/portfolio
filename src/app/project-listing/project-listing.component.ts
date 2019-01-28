4b0262fd62cbd619f9c6b40d6e0d6d309a72aaaa
spelka <sebastian.pelka@hotmail.com>
2019-01-28 14:30:26 -0800

    Added project listing component

---
+++ src/app/project-listing/project-listing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-project-listing',
+  templateUrl: './project-listing.component.html',
+  styleUrls: ['./project-listing.component.css']
+})
+export class ProjectListingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
