edcdf1aceb13ac5bc5fa6f4b0f7b32e101699740
spelka <sebastian.pelka@hotmail.com>
2019-01-28 18:26:41 -0800

    Project items are now being displayed as a project-item component

--- src/app/project-item/project-item.component.ts
+++ src/app/project-item/project-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-project-item',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectItemComponent implements OnInit {
 
+  //accept binding 'project' and assign it to a local property project: any
+  @Input('project') project: any;
   constructor() { }
 
   ngOnInit() {
