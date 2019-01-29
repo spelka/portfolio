d22cac97915e57753027d12e5b4415424f420c86
spelka <sebastian.pelka@hotmail.com>
2019-01-28 17:34:08 -0800

    added JSON entry for project link and replaced # in project-listing component

--- src/app/data/projects.ts
+++ src/app/data/projects.ts
@@ -4,21 +4,24 @@ export const projects = [
         "heading": "Heading 1",
         "name": "Project 1",
         "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        "image": "project-1"
+        "image": "700x300",
+        "link": "#"
     },
     {
         "id": 2,
         "heading": "Heading 2",
         "name": "Project 2",
         "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        "image": "project-2"
+        "image": "700x300",
+        "link": "#"
     },
     {
         "id": 3,
         "heading": "Heading 3",
         "name": "Project 3",
         "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        "image": "project-3"
+        "image": "700x300",
+        "link": "#"
     },
 
 ];
