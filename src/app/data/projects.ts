725d248fa8c7894967e0a4b9120ded89deaab50e
spelka <sebastian.pelka@hotmail.com>
2019-01-28 17:57:58 -0800

    Added Tea-Da

--- src/app/data/projects.ts
+++ src/app/data/projects.ts
@@ -1,10 +1,10 @@
 export const projects = [
     {
         "id": 1,
-        "heading": "Heading 1",
-        "name": "Project 1",
-        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        "image": "700x300",
+        "heading": "Tea-Da",
+        "name": "A tea timer app for Android",
+        "description": "Tea-Da is a simple-yet-elegant app that helps you make sure you never over-steep your tea again. Select your tea type from the menu, set the timer, and let Tea-Da do the rest!",
+        "image": "tea-da",
         "link": "#"
     },
     {
@@ -12,7 +12,7 @@ export const projects = [
         "heading": "Heading 2",
         "name": "Project 2",
         "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        "image": "700x300",
+        "image": "tea-da-2",
         "link": "#"
     },
     {
