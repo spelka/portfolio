97055ad54f83b6f33ef048548f0b7703c0cf2ea8
spelka <sebastian.pelka@hotmail.com>
2019-01-28 17:28:51 -0800

    Added data for each heading item

--- src/app/data/projects.ts
+++ src/app/data/projects.ts
@@ -1,18 +1,21 @@
 export const projects = [
     {
         "id": 1,
+        "heading": "Heading 1",
         "name": "Project 1",
         "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
         "image": "project-1"
     },
     {
         "id": 2,
+        "heading": "Heading 2",
         "name": "Project 2",
         "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
         "image": "project-2"
     },
     {
         "id": 3,
+        "heading": "Heading 3",
         "name": "Project 3",
         "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
         "image": "project-3"
