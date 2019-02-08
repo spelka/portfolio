b5f070f7d44089f948e3f62bffb9b499530b6604
spelka <sebastian.pelka@hotmail.com>
2019-02-07 19:01:29 -0800

    Added experience-listing component

---
+++ src/app/experience-listing/experience-listing.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-experience-listing',
+  templateUrl: './experience-listing.component.html',
+  styleUrls: ['./experience-listing.component.css']
+})
+export class ExperienceListingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  
+}
