b2e147767781b2204f1e8e1e2788e188ee651ce0
spelka <sebastian.pelka@hotmail.com>
2019-02-08 09:11:54 -0800

    updated project listing with google play store link, and buttons for each project how have a description to provide context to the user

--- src/app/data/projects.ts
+++ src/app/data/projects.ts
@@ -5,7 +5,8 @@ export const projects = [
         "name": "A tea timer app for Android",
         "description": "Tea-Da is a simple-yet-elegant app that helps you make sure you never over-steep your tea again. Select your tea type from the menu, set the timer, and let Tea-Da do the rest!",
         "image": "tea-da",
-        "link": "#"
+        "link": "https://play.google.com/apps/testing/ca.sebon.tea_da",
+        "linkdescription": "Google Play Store"
     },
     {
         "id": 2,
@@ -13,7 +14,8 @@ export const projects = [
         "name": "Web-based To-Do List",
         "description": "Plan our your daily tasks with this handy web-based to-do list. Add and remove items as needed. Created with Node.js, Angular7, and Boostrap4.",
         "image": "todo-app",
-        "link": "https://github.com/spelka/todo-app"
+        "link": "https://github.com/spelka/todo-app",
+        "linkdescription" : "View on Github" 
     },
     {
         "id": 3,
@@ -21,7 +23,8 @@ export const projects = [
         "name": "Web-based Weather Forecast",
         "description": "A minimalistic 5-day forecast for major cities across Canada. Accurate weather outlook is provided to you by the Dark Sky API. Created with Node.js, Angular7, and Bootstrap4.",
         "image": "weather-app",
-        "link": "https://github.com/spelka/weather-web-app"
+        "link": "https://github.com/spelka/weather-web-app",
+        "linkdescription" : "View on Github" 
     },
 
 ];
