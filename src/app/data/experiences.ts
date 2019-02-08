0d19acd8060b7ed57b3d7503cd49c697407d3725
spelka <sebastian.pelka@hotmail.com>
2019-02-07 22:35:14 -0800

    Added JSON file of work experience

---
+++ src/app/data/experiences.ts
@@ -0,0 +1,42 @@
+export const experiences = [
+    {
+        'companyName' : 'MDA Corporation',
+        'title' : 'DevOps Developer (Junior Systems Engineer)',
+        'startDate' : 'February 2018',
+        'endDate' : 'July 2018',
+        'responsibilities' : [
+            'Using Saltstack for configuration management, application, and custom software deployment.',
+            'Created scripts using Saltstack and Python to deploy third party software and custom MDA Software to Windows and Linux Environments.',
+            'Created Python and Shell scripts.',
+            'Source Control Configurations and Scripts through GIT.',
+            'Participated in an Agile Development Environment.',
+            'Infrastructure automation for both Windows and Linux.'
+        ]
+    },
+    {
+        'companyName' : 'MDA Corporation',
+        'title' : 'Integration and Test (Junior Systems Engineer)',
+        'startDate' : 'July 2016',
+        'endDate' : 'February 2018',
+        'responsibilities' : [
+            'Learned enterprise ecosystem of software to enable ground systems to communicate with antennas and satellites.',
+            'Deep understanding in communication between software.',
+            'Test, Debug various types of Software on different platforms (Windows based, Linux based).',
+            'Participate in software system selloff cycle (dry Run, sell off, onsite deployment, customer training).',
+            'Test Engineer (investigate customer systems remotely, reproduce errors locally, debug and pinpoint point of failure, work with developers to resolve bugs).'
+        ]
+    },
+    {
+        'companyName' : 'Cardant Software, Inc.',
+        'title' : 'Software Developer',
+        'startDate' : 'May 2015',
+        'endDate' : 'June 2016',
+        'responsibilities' : [
+            'Designed and implemented an automated testing framework.',
+            'Implemented Globalization and Localization requirements as required by clients.',
+            'EMV terminal and host certification with VISA, MasterCard, American Express, and Discover.',
+            'Providing timely and thorough analysis and support for clients.',
+            'Detailed and thorough testing of software before release deadlines.'
+        ]
+    }
+]
